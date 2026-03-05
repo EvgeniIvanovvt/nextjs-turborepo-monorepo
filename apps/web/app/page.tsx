5d9f365905cbbd8ebd6be72e707dffd133ff1b5f
Evgeni Ivanov <styler@abv.bg>
2026-03-05 12:31:08 +0200

    Update page.tsx

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -21,7 +21,7 @@ const ThemeImage = (props: Props) => {
 export default function Home() {
   return (
     <div className={styles.page}>
-      <h1 className={styles.title}>Welcome to Next.js!</h1>
+      <h1 className={styles.title}>Welcome to Next.js! TEST</h1>
     </div>
   );
 }
